0e7fe797728d407d424a75fb930d68de3decb42a
trek <johnpatrickgalanido@gmail.com>
2026-07-15 19:26:18 +0800

    email sender from fixed

--- backend/utils/emailSender.js
+++ backend/utils/emailSender.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
     try {
         const { data, error } = await resend.emails.send({
-            from: 'TapAware <onboarding@resend.dev>',
+            from: 'TapAware <noreply@tapaware.online>',
             to,
             subject,
             html,
